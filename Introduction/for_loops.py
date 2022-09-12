226b96ac860adf09594843ea2ac72969f294150f
Shirzart Enwer <shirzartenwer@gmail.com>
2022-09-12 18:46:10 +0200

    feat: include the python version

---
+++ Introduction/for_loops.py
@@ -0,0 +1,22 @@
+import sys
+import random
+from time import *
+
+
+
+n = 1024
+
+A = [[random.random() for row in range(n)] for col in range(n)]
+B = [[random.random() for row in range(n)] for col in range(n)]
+C = [[0 for row in range(n)] for col in range(n)]
+
+
+start = time()
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            C[i][j] += A[i][k]*B[k][j]
+            
+end = time()
+
+print(f'The computation time is {end - start}')
