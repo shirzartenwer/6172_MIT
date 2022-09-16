44cd9f2fa2cf19571817aa53eb107fefc1bd5d0e
Shirzart Enwer <shirzartenwer@gmail.com>
2022-09-16 10:09:50 +0200

    feat: add the date Struct from lecture

---
+++ L2_new_bently_rule/data_types.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include<string.h>
+
+struct Date{
+    int year: 13;
+    int month: 4;
+    int date: 5;
+}date_t;
+
+
+int main (){
+    date_t.year = 1993;
+    date_t.month = 9;
+    date_t.date = 24;
+
+    return 0;
+}
+
+//#include <iostream>
+//#include <string>
+//#include <sstream>
+//using namespace std;
+
+//struct movies_t {
+  //string title;
+  //int year;
+//} mine, yours;
+
+//void printmovie (movies_t movie);
+
+//int main ()
+//{
+  //string mystr;
+
+  //mine.title = "2001 A Space Odyssey";
+  //mine.year = 1968;
+
+  //cout << "Enter title: ";
+  //getline (cin,yours.title);
+  //cout << "Enter year: ";
+  //getline (cin,mystr);
+  //stringstream(mystr) >> yours.year;
+
+  //cout << "My favorite movie is:\n ";
+  //printmovie (mine);
+  //cout << "And yours is:\n ";
+  //printmovie (yours);
+  //return 0;
+//}
+
+//void printmovie (movies_t movie)
+//{
+  //cout << movie.title;
+  //cout << " (" << movie.year << ")\n";
+//}
